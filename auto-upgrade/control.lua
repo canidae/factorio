c8c194c6e3a373fd6bffb3460010aeb51b0d9d6a
Vidar Wahlberg <canidae@exent.net>
2016-10-03 00:26:42 +0200

    auto upgrade beginning to look like something

---
+++ auto-upgrade/control.lua
@@ -0,0 +1,392 @@
+auto_upgrade_config_version = 1
+
+function init()
+    if not global.auto_upgrade then
+        global.auto_upgrade = {
+            enabled = true
+        }
+    end
+    for _, force in pairs(game.forces) do
+        if not global.auto_upgrade[force.name] or global.auto_upgrade[force.name].version ~= auto_upgrade_version then
+            global.auto_upgrade[force.name] = {
+                version = auto_upgrade_version,
+                upgrade = {},
+                roboports = {}
+            }
+        else
+            -- check that entities and modules still exist in game (and remove from settings if not)
+            for entityname, settings in pairs(global.auto_upgrade[force.name].upgrade) do
+                if not game.entity_prototypes[entityname] then
+                    global.auto_upgrade[force.name].upgrade[entityname] = nil
+                else
+                    for modulename, _ in pairs(settings.modules) do
+                        if not game.item_prototypes[modulename] then
+                            settings.modules[modulename] = nil
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+function getConfig(force)
+    return global.auto_upgrade[force.name]
+end
+
+function onBuiltEntity(event)
+    local entity = event.created_entity
+    local config = getConfig(entity.force)
+    if entity.logistic_cell then
+        -- seems like a roboport
+        config.roboports[#config.roboports + 1] = entity
+    end
+    if config.upgrade[entity.name] then
+        config.upgrade[entity.name].entities[#config.upgrade[entity.name].entities + 1] = entity
+    end
+end
+
+function findBestModules(inventory, requested, include_modules)
+    -- note: "inventory" may be LuaInventory or LuaLogisticsNetwork, they both provide (nearly) identical "get_item_count()" methods
+    if not inventory or not requested then
+        return nil
+    end
+    local result = {}
+    for module, count in pairs(requested) do
+        local pre_item_name, level_cap = string.match(module, "^(.*)-(%d)$")
+        if level_cap then
+            -- likely multiple levels of this module, search in inventory how many are available, starting at highest level
+            local remaining = count
+            for i = level_cap, 1, -1 do
+                local item_name = i > 1 and (pre_item_name .. "-" .. i) or pre_item_name
+                if game.item_prototypes[item_name] then
+                    local count = inventory.get_item_count(item_name)
+                    if include_modules and include_modules[item_name] then
+                        count = count + include_modules[item_name]
+                    end
+                    if count > 0 then
+                        local min = math.min(count, remaining)
+                        result[#result + 1] = {
+                            item = item_name,
+                            count = min
+                        }
+                        remaining = remaining - min
+                        if remaining <= 0 then
+                            break
+                        end
+                    end
+                end
+            end
+        else
+            -- doesn't look like there are multiple levels of this module
+            local count = inventory.get_item_count(module)
+            if count > 0 then
+                result[#result + 1] = {
+                    item = module,
+                    count = min
+                }
+            end
+        end
+    end
+    return result
+end
+
+function findNetworkOrPlayerInventory(entity)
+    local config = getConfig(entity.force)
+    for i = #config.roboports, 1, -1 do
+        local roboport = config.roboports[i]
+        if roboport.valid then
+            local range = roboport.logistic_cell.construction_radius
+            if range > 0 then
+                local position = roboport.position
+                if entity.position.x >= position.x - range and entity.position.x <= position.x + range then
+                    if entity.position.y >= position.y - range and entity.position.y <= position.y + range then
+                        -- entity within reach of this roboport
+                        if roboport.logistic_network.available_construction_robots > 0 then
+                            -- and we got available construction robots
+                            return roboport.logistic_network
+                        end
+                    end
+                end
+            end
+        else
+            table.remove(config.roboports, i)
+        end
+    end
+
+    -- TODO: check if player has personal roboport, construction robots and is within reach of entity
+end
+
+function upgradeEntityIfNecessary(entity)
+    local config = getConfig(entity.force)
+    local upgrade = config.upgrade[entity.name]
+    if not upgrade then
+        return
+    end
+    local network = findNetworkOrPlayerInventory(entity)
+    if not network then
+        return
+    end
+    local area = {{entity.position.x - 0.5, entity.position.y - 0.5}, {entity.position.x + 0.5, entity.position.y + 0.5}}
+    local proxy = entity.surface.find_entities_filtered{area = area, name = "item-request-proxy"}
+    if proxy[1] ~= nil then
+        -- there's an "item-request-proxy" for this entity, don't attempt to upgrade
+        return
+    end
+
+    local replace = false
+    if upgrade.target and entity.name ~= upgrade.target and network.get_item_count(upgrade.target) > 0 then
+        -- upgrading from one entity to another, and we got the target entity in our logistic network
+        replace = true
+    end
+    local module_inventory = entity.get_module_inventory()
+    local current_modules = module_inventory and module_inventory.get_contents() or nil
+    local best_modules = (replace or upgrade.modules) and findBestModules(network, upgrade.modules, current_modules)
+    if not replace and upgrade.modules and current_modules then
+        -- possibly only upgrading modules, check if any modules can be upgraded
+        for _, module in pairs(best_modules) do
+            if current_modules[module.item] ~= module.count then
+                replace = true
+                break
+            end
+        end
+        if replace then
+            local current_count = 0
+            for modulename, count in pairs(current_modules) do
+                current_count = current_count + count
+            end
+            local best_count = 0
+            for _, module in pairs(best_modules) do
+                best_count = best_count + module.count
+            end
+            if best_count > current_count and not module_inventory.can_insert({name = best_modules[1].item}) then
+                -- can't place any more modules into this entity
+                replace = false
+            end
+        end
+    end
+    if replace then
+        replaceEntity(entity, upgrade.target or entity.name, best_modules)
+    end
+end
+
+function replaceEntity(entity, target_prototype, modules)
+    entity.order_deconstruction(entity.force)
+    local new_entity = game.surfaces[entity.surface.name].create_entity{name = "entity-ghost", inner_name = target_prototype, direction = entity.direction, position = entity.position, force = entity.force}
+    if modules then
+        new_entity.item_requests = modules
+    end
+    if entity.type == "crafting-machine" and entity.recipe then
+        new_entity.recipe = entity.recipe
+    end
+end
+
+function tellPlayer(player, message)
+    player.print("[Auto Upgrade] " .. message) -- TODO: localization
+end
+
+gui = {
+    toggleGui = function(player)
+        local force = player.force
+        local config = getConfig(force)
+        if player.gui.top.auto_upgrade_gui then
+            player.gui.top.auto_upgrade_gui.destroy()
+
+            -- update tracked entities for player force
+            for _, surface in pairs(game.surfaces) do
+                for entityname, settings in pairs(config.upgrade) do
+                    for _, entity in pairs(surface.find_entities_filtered{name = entityname, force = force.name}) do
+                        settings.entities[#settings.entities + 1] = entity
+                    end
+                end
+                -- and roboports
+                for _, entity in pairs(surface.find_entities_filtered{name = "roboport", force = force.name}) do
+                    config.roboports[#config.roboports + 1] = entity
+                end
+            end
+        else
+            local frame = player.gui.top.add{
+                type = "frame",
+                name = "auto_upgrade_gui",
+                direction = "vertical",
+                caption = {"gui.title"}
+            }
+            local frameflow = frame.add{
+                type = "flow",
+                style = "auto_upgrade_list_flow",
+                name = "flow",
+                direction = "vertical"
+            }
+
+            -- checkboxes
+            frameflow.add{type = "checkbox", style = "auto_upgrade_checkbox", name = "auto_upgrade_enabled", caption = {"gui.enabled"}, tooltip = {"gui.enabled_tooltip"}, state = config.enabled or false}
+
+            -- add "<new entity>" entry
+            local entryflow = frameflow.add{type = "flow", direction = "horizontal"}
+            entryflow.add{type = "sprite-button", style = "auto_upgrade_sprite_button", name = "auto_upgrade_add", sprite = "auto_upgrade_add"}
+            entryflow.add{type = "label", style = "auto_upgrade_label", caption = {"gui.add_entity"}}
+
+            -- scrollpane
+            local scrollpane = frameflow.add{
+                type = "scroll-pane",
+                name = "scrollpane",
+                horizontal_scroll_policy = "never",
+                vertical_scroll_policy = "auto"
+            }
+            scrollpane.style.top_padding = 5
+            scrollpane.style.bottom_padding = 5
+            scrollpane.style.maximal_height = 192
+            gui.updateUpgradeList(scrollpane, force)
+        end
+    end,
+
+    onClick = function(event)
+        local player = game.players[event.player_index]
+        local force = player.force
+        local config = getConfig(force)
+        local name = event.element.name
+        if name == "auto_upgrade_enabled" then
+            config.enabled = event.element.state
+        elseif string.match(name, "^auto_upgrade_add$") then
+            local stack = player.cursor_stack
+            if stack.valid_for_read and not config.upgrade[stack.name] and game.entity_prototypes[stack.name] then
+                config.upgrade[stack.name] = {
+                    modules = {},
+                    entities = {},
+                    index = 1
+                }
+            end
+        else
+            local entityname = string.match(name, "^auto_upgrade_delete_(.*)$")
+            if entityname then
+                config.upgrade[entityname] = nil
+            end
+
+            local entityname = string.match(name, "^auto_upgrade_target_(.*)$")
+            if entityname then
+                local stack = player.cursor_stack
+                if stack.valid_for_read and entityname ~= stack.name then
+                    local e_cb = game.entity_prototypes[entityname].collision_box
+                    local t_cb = game.entity_prototypes[stack.name].collision_box
+                    if e_cb.left_top.x == t_cb.left_top.x and e_cb.left_top.y == t_cb.left_top.y and e_cb.right_bottom.x == t_cb.right_bottom.x and e_cb.right_bottom.y == t_cb.right_bottom.y then
+                        config.upgrade[entityname].target = stack.name
+                    else
+                        tellPlayer(player, "Can't upgrade " .. entityname .. " to " .. stack.name .. ", not the same entity size") -- TODO: localization
+                    end
+                else
+                    config.upgrade[entityname].target = nil
+                end
+            end
+
+            local entityname = string.match(name, "^auto_upgrade_add_module_(.*)$")
+            if entityname then
+                local stack = player.cursor_stack
+                if stack.valid_for_read and game.item_prototypes[stack.name].type == "module" then
+                    -- check if we got no more than 8 modules set up for entity
+                    local module_count = 0
+                    for modulename, count in pairs(config.upgrade[entityname].modules) do
+                        module_count = module_count + count
+                    end
+                    if module_count < 8 then
+                        config.upgrade[entityname].modules[stack.name] = (config.upgrade[entityname].modules[stack.name] or 0) + 1
+                    end
+                end
+            end
+
+            local entityname, modulename = string.match(name, "^auto_upgrade_remove_module_(.*)_%d+_(.*)$")
+            if entityname then
+                if config.upgrade[entityname].modules[modulename] > 1 then
+                    config.upgrade[entityname].modules[modulename] = config.upgrade[entityname].modules[modulename] - 1
+                else
+                    config.upgrade[entityname].modules[modulename] = nil
+                end
+                -- if we happen to hold a module on cursor, add that
+                local stack = player.cursor_stack
+                if stack.valid_for_read and game.item_prototypes[stack.name].type == "module" then
+                    config.upgrade[entityname].modules[stack.name] = (config.upgrade[entityname].modules[stack.name] or 0) + 1
+                end
+            end
+        end
+        if player.gui.top.auto_upgrade_gui then
+            gui.updateUpgradeList(player.gui.top.auto_upgrade_gui.flow.scrollpane, force)
+        end
+    end,
+
+    updateUpgradeList = function(scrollpane, force)
+        if scrollpane.table then
+            scrollpane.table.destroy()
+        end
+
+        -- add table
+        local table = scrollpane.add{
+            type = "table",
+            name = "table",
+            colspan = 3
+        }
+
+        -- add list of entities to upgrade
+        local config = getConfig(force)
+        for entityname, settings in pairs(config.upgrade) do
+            local col1flow = table.add{type = "flow", direction = "horizontal"}
+            col1flow.add{type = "sprite-button", style = "auto_upgrade_sprite_button", name = "auto_upgrade_delete_" .. entityname, sprite = "auto_upgrade_delete"}
+            col1flow.add{type = "label", style = "auto_upgrade_label", caption = game.entity_prototypes[entityname].localised_name}
+
+            -- button for adding/removing modules
+            local col2flow = table.add{type = "flow", direction = "horizontal"}
+            col2flow.add{type = "sprite-button", style = "auto_upgrade_sprite_button", name = "auto_upgrade_add_module_" .. entityname, sprite = "auto_upgrade_add_module"}
+            if settings.modules then
+                local button_id = 0
+                for modulename, count in pairs(settings.modules) do
+                    for i = 1, count do
+                        button_id = button_id + 1
+                        col2flow.add{type = "sprite-button", style = "auto_upgrade_sprite_button", name = "auto_upgrade_remove_module_" .. entityname .. "_" .. button_id .. "_" .. modulename, sprite = "auto_upgrade_module_" .. modulename}
+                    end
+                end
+            end
+
+            -- button for upgrading entity
+            local col3flow = table.add{type = "flow", direction = "horizontal"}
+            col3flow.add{type = "sprite-button", style = "auto_upgrade_sprite_button", name = "auto_upgrade_target_" .. entityname, sprite = "auto_upgrade_target"}
+            col3flow.add{type = "label", style = "auto_upgrade_label", caption = settings.target and game.entity_prototypes[settings.target].localised_name or {"gui.upgrade_target"}}
+        end
+    end
+}
+
+
+script.on_init(init)
+script.on_configuration_changed(init)
+script.on_event(defines.events.on_force_created, init)
+script.on_event(defines.events.on_built_entity, onBuiltEntity)
+script.on_event(defines.events.on_robot_built_entity, onBuiltEntity)
+script.on_event(defines.events.on_gui_click, gui.onClick)
+
+script.on_event(defines.events.on_tick, function(event)
+    if game.tick % 60 > 0 then
+        return
+    end
+    -- TODO: upgrade more than 1 entity (of each kind) at a time. check how many we can upgrade (in storage), how many available construction robots there are. then increase time between each upgrade check
+    -- TODO: remove fully upgraded entities
+    for _, force in pairs(game.forces) do
+        local config = getConfig(force)
+        for entityname, settings in pairs(config.upgrade) do
+            settings.index = settings.index - 1
+            if settings.index < 1 then
+                settings.index = #settings.entities
+            end
+            if settings.index > 0 then
+                local entity = settings.entities[settings.index]
+                if entity.valid then
+                    upgradeEntityIfNecessary(entity)
+                else
+                    table.remove(settings.entities, settings.index)
+                end
+            end
+        end
+    end
+end)
+
+-- keybinding hooks
+script.on_event("auto_upgrade_toggle", function(event)
+    local player = game.players[event.player_index]
+    gui.toggleGui(player)
+end)
---
+++ auto-upgrade/data.lua
@@ -0,0 +1,93 @@
+data.raw["gui-style"].default["auto_upgrade_list_flow"] = {
+    type = "flow_style",
+    parent = "flow_style",
+    vertical_spacing = 0
+}
+
+data.raw["gui-style"].default["auto_upgrade_sprite_button"] = {
+    type = "button_style",
+    parent = "button_style",
+    width = 32,
+    height = 32,
+    top_padding = 0,
+    right_padding = 0,
+    bottom_padding = 0,
+    left_padding = 0,
+    left_click_sound = {
+        {
+            filename = "__core__/sound/gui-click.ogg",
+            volume = 1
+        }
+    }
+}
+
+data.raw["gui-style"].default["auto_upgrade_checkbox"] = {
+    type = "checkbox_style",
+    parent = "checkbox_style",
+    bottom_padding = 12
+}
+
+data.raw["gui-style"].default["auto_upgrade_label"] = {
+    type = "label_style",
+    parent = "label_style",
+    top_padding = 6
+}
+
+data:extend({
+	-- keybindings
+	{
+		type = "custom-input",
+		name = "auto_upgrade_toggle",
+		key_sequence = "SHIFT + u"
+	},
+
+	-- sprites
+	{
+		type = "sprite",
+		name = "auto_upgrade_add",
+		filename = "__auto-upgrade__/graphics/add-icon.png",
+		priority = "extra-high-no-scale",
+		width = 32,
+		height = 32
+	},
+	{
+		type = "sprite",
+		name = "auto_upgrade_delete",
+		filename = "__auto-upgrade__/graphics/delete.png",
+		priority = "extra-high-no-scale",
+		width = 32,
+		height = 32
+	},
+	{
+		type = "sprite",
+		name = "auto_upgrade_add_module",
+		filename = "__auto-upgrade__/graphics/add-module.png",
+		priority = "extra-high-no-scale",
+		width = 32,
+		height = 32
+	},
+	{
+		type = "sprite",
+		name = "auto_upgrade_target",
+		filename = "__auto-upgrade__/graphics/goto-icon.png",
+		priority = "extra-high-no-scale",
+		width = 32,
+		height = 32
+	}
+})
+
+-- dynamically add sprites for modules
+for _, module in pairs(data.raw.module) do
+    if module.icon then
+        data:extend({
+            {
+                type = "sprite",
+                name = "auto_upgrade_module_" .. module.name,
+                filename = module.icon,
+                priority = "extra-high-no-scale",
+                width = 32,
+                height = 32
+            }
+        })
+    end
+end
