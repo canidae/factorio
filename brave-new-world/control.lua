8ab45562d111ecbe81d66cb7edb251ec53c94f76
Vidar Wahlberg <canidae@exent.net>
2016-10-13 23:48:58 +0200

    bnw: went far one direction today, then backtracked

--- brave-new-world/control.lua
+++ brave-new-world/control.lua
@@ -11,47 +11,87 @@ script.on_event(defines.events.on_player_created, function(event)
     if character then
         character.destroy()
     end
-    player.cheat_mode = true
+    player.force.manual_mining_speed_modifier = -1
+    player.force.manual_crafting_speed_modifier = -1
+    player.insert{name = "deconstruction-planner", count = 1}
 
     -- build initial structures
-    -- TODO: landfill starting area
     local surface = player.surface
-    surface.create_entity{name = "constant-combinator", position = {0, 6}, force = player.force} -- TODO: this will be the player habitat that may not be destroyed
-    local roboport = surface.create_entity{name = "roboport", position = {0, 0}, force = player.force}
+    -- place dirt beneath structures
+    local tiles = {}
+    for x = -11, 7 do
+        for y = -9, 3 do
+            table.insert(tiles, {name = "grass-dry", position = {x, y}})
+        end
+    end
+    surface.set_tiles(tiles)
+    -- remove trees/stones
+    local entities = surface.find_entities_filtered{area = {{-13, -11}, {9, 5}}, force = "neutral"}
+    for _, entity in pairs(entities) do
+        entity.destroy()
+    end
+    -- place walls
+    for x = -9, -4 do
+        surface.create_entity{name = "stone-wall", position = {x, -7}, force = player.force}
+        surface.create_entity{name = "stone-wall", position = {x, 1}, force = player.force}
+    end
+    for y = -7, 1 do
+        surface.create_entity{name = "stone-wall", position = {-9, y}, force = player.force}
+        surface.create_entity{name = "stone-wall", position = {-4, y}, force = player.force}
+    end
+    -- roboport
+    local roboport = surface.create_entity{name = "roboport", position = {-6, -4}, force = player.force}
+    roboport.minable = false
     local roboport_inventory = roboport.get_inventory(defines.inventory.roboport_robot)
     roboport_inventory.insert{name = "construction-robot", count = 10}
     roboport_inventory.insert{name = "logistic-robot", count = 10}
     roboport_inventory = roboport.get_inventory(defines.inventory.roboport_material)
     roboport_inventory.insert{name = "repair-pack", count = 10}
-    surface.create_entity{name = "medium-electric-pole", position = {0, 3}, force = player.force}
-    local chest = surface.create_entity{name = "logistic-chest-storage", position = {-1, 3}, force = player.force}
+    -- habitat
+    local habitat = surface.create_entity{name = "player-habitat", position = {-7, -1}, force = player.force}
+    habitat.operable = false
+    -- electric pole
+    local electric_pole = surface.create_entity{name = "medium-electric-pole", position = {-5, -2}, force = player.force}
+    electric_pole.minable = false
+    -- chests
+    local chest = surface.create_entity{name = "logistic-chest-storage", position = {-5, -1}, force = player.force}
+    chest.minable = false
     local chest_inventory = chest.get_inventory(defines.inventory.chest)
     chest_inventory.insert{name = "iron-plate", count = 1000}
     chest_inventory.insert{name = "copper-plate", count = 1000}
-    chest_inventory.insert{name = "coal", count = 250}
-    chest_inventory.insert{name = "stone", count = 250}
-    chest_inventory.insert{name = "small-electric-pole", count = 10}
-    chest_inventory.insert{name = "transport-belt", count = 50}
-    chest_inventory.insert{name = "inserter", count = 10}
-    chest_inventory.insert{name = "assembling-machine-1", count = 5}
-    chest_inventory.insert{name = "electric-mining-drill", count = 5}
-    chest_inventory.insert{name = "stone-furnace", count = 5}
+    chest_inventory.insert{name = "coal", count = 200}
+    chest_inventory.insert{name = "stone", count = 50}
+    chest_inventory.insert{name = "transport-belt", count = 250}
+    chest_inventory.insert{name = "pipe", count = 50}
+    chest_inventory.insert{name = "medium-electric-pole", count = 50}
+    chest_inventory.insert{name = "inserter", count = 20}
+    chest_inventory.insert{name = "steam-engine", count = 1}
+    chest_inventory.insert{name = "assembling-machine-1", count = 4}
+    chest_inventory.insert{name = "electric-mining-drill", count = 4}
+    chest_inventory.insert{name = "stone-furnace", count = 4}
+    chest_inventory.insert{name = "roboport", count = 4}
     chest_inventory.insert{name = "logistic-chest-storage", count = 5}
     chest_inventory.insert{name = "logistic-chest-active-provider", count = 5}
     chest_inventory.insert{name = "logistic-chest-passive-provider", count = 5}
     chest_inventory.insert{name = "logistic-chest-requester", count = 5}
-    surface.create_entity{name = "solar-panel", position = {6, 0}, force = player.force}
-    surface.create_entity{name = "solar-panel", position = {6, 6}, force = player.force}
-    surface.create_entity{name = "solar-panel", position = {9, 6}, force = player.force}
-    surface.create_entity{name = "solar-panel", position = {12, 0}, force = player.force}
-    surface.create_entity{name = "solar-panel", position = {12, 3}, force = player.force}
-    surface.create_entity{name = "solar-panel", position = {12, 6}, force = player.force}
-    surface.create_entity{name = "medium-electric-pole", position = {8, 2}, force = player.force}
-    surface.create_entity{name = "accumulator", position = {6, 4}, force = player.force}
-    surface.create_entity{name = "accumulator", position = {8, 4}, force = player.force}
-    surface.create_entity{name = "accumulator", position = {10, 0}, force = player.force}
-    surface.create_entity{name = "accumulator", position = {10, 2}, force = player.force}
-    surface.create_entity{name = "accumulator", position = {10, 4}, force = player.force}
+    chest_inventory.insert{name = "lab", count = 1}
+    chest = surface.create_entity{name = "logistic-chest-storage", position = {-5, 0}, force = player.force}
+    chest.minable = false
+    -- solar panels and accumulators
+    surface.create_entity{name = "solar-panel", position = {-2, -6}, force = player.force}
+    surface.create_entity{name = "solar-panel", position = {-2, 0}, force = player.force}
+    surface.create_entity{name = "solar-panel", position = {1, 0}, force = player.force}
+    surface.create_entity{name = "solar-panel", position = {4, -6}, force = player.force}
+    surface.create_entity{name = "solar-panel", position = {4, -3}, force = player.force}
+    surface.create_entity{name = "solar-panel", position = {4, 0}, force = player.force}
+    surface.create_entity{name = "medium-electric-pole", position = {0, -4}, force = player.force}
+    surface.create_entity{name = "accumulator", position = {-2, -2}, force = player.force}
+    surface.create_entity{name = "accumulator", position = {0, -2}, force = player.force}
+    surface.create_entity{name = "accumulator", position = {2, -6}, force = player.force}
+    surface.create_entity{name = "accumulator", position = {2, -4}, force = player.force}
+    surface.create_entity{name = "accumulator", position = {2, -2}, force = player.force}
+
+    -- TODO: insert some stuff in player inventory too (inserters, belts, lab, steam engine, ...)
 end)
 
 script.on_event(defines.events.on_put_item, function(event)
@@ -64,7 +104,6 @@ script.on_event(defines.events.on_put_item, function(event)
         global.brave_new_world[player.name].cursor_stack = entity.ghost_name
         player.cursor_stack.clear()
     end
-    -- TODO: what if we instead "convert" cursor item to blueprint, and back again in on_built_entity?
 end)
 
 script.on_event(defines.events.on_built_entity, function(event)
@@ -86,6 +125,7 @@ script.on_event(defines.events.on_built_entity, function(event)
 end)
 
 script.on_event(defines.events.on_tick, function(event)
+    -- TODO: can we prevent exploration by setting player.position? - game.players[1].teleport{x, y}
     for playername, player in pairs(global.brave_new_world) do
         if player.cursor_stack then
             game.players[playername].cursor_stack.set_stack{name = player.cursor_stack, count = 1}
