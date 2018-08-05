36bd197d68145d5f642a9c41788c8ebce01c9cef
Vidar Wahlberg <canidae@exent.net>
2018-08-05 13:31:40 +0200

    AR: better support for research ingredients with multiple icons

--- auto-research/data-final-fixes.lua
+++ auto-research/data-final-fixes.lua
@@ -1,11 +1,11 @@
 -- dynamically add sprites for tools (to display research ingredients)
 for _, tool in pairs(data.raw.tool) do
-    if tool.icon then
+    if tool.icon or tool.icons then
         data:extend({
             {
                 type = "sprite",
                 name = "auto_research_tool_" .. tool.name,
-                filename = tool.icon,
+                filename = tool.icon or (tool.icons[1] and tool.icons[1].icon) or nil,
                 priority = "extra-high-no-scale",
                 width = 32,
                 height = 32
